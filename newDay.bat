d41d1a79b5d41679094dffcb6b677f7cd8d0eda7
tomaszorda@outlook.com <tomaszorda@outlook.com>
2024-12-10 15:00:22 +0100

    Add .bat script for creating files for a given day

---
+++ newDay.bat
@@ -0,0 +1,17 @@
+@echo off
+
+set DayNumber=%1
+
+type NUL > Day_%DayNumber%.txt
+type NUL > Day_%DayNumber%_test.txt
+
+echo. >> src\day%DayNumber%part1.rs
+echo pub fn solution(input: String) -^> String { >> src\day%DayNumber%part1.rs
+echo. >> src\day%DayNumber%part1.rs
+echo     format!("{:?}",input) >> src\day%DayNumber%part1.rs
+echo } >> src\day%DayNumber%part1.rs
+
+copy src\day%DayNumber%part1.rs src\day%DayNumber%part2.rs
+
+:: Output success message
+echo Files created and copied successfully.
