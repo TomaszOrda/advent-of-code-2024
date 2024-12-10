a46323941392e2bd562def8354beaf010ea51c21
tomaszorda@outlook.com <tomaszorda@outlook.com>
2024-12-10 14:41:07 +0100

    Clean up main.rs; set Example to day1task1r; Ignore in further commits unless  manually staged

--- src/main.rs
+++ src/main.rs
@@ -5,14 +5,12 @@ macro_rules! mod_day_and_task{
     }
 }
 
-mod_day_and_task!(day7part2r);
+mod_day_and_task!(day1part1r);
 fn main(){
     let args = &mut std::env::args();
     args.next();
     let input_file: String = args.next().unwrap_or_else(|| "No input file provided".to_string());
     let input: String = std::fs::read_to_string(input_file).unwrap();
-// test all solutions
-// send to github
-// Create a template
+
     println!("{}", solution(input));
 }
